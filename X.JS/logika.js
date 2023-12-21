9069d2ad7e1500f574d42829c2fe20a0d65e9361
KonradPietrzak <kondzio1333@o2.pl>
2023-12-21 09:44:43 +0000

    Espanol
    Fixing path to JS and CSS

--- X.JS/logika.js
+++ X.JS/logika.js
@@ -16,3 +16,20 @@ const hoverContent = hoverDiv.querySelector('.hover-content');
 button.addEventListener('click', function() {
   hoverDiv.classList.toggle('active');
 })
+//Podobne też ciekawe
+/*const div1 = document.getElementById("div1");
+const div2 = document.getElementById("div2");
+
+div1.addEventListener("mouseover", function() {
+  div2.classList.add("highlight");
+});
+
+div1.addEventListener("mouseout", function() {
+  div2.classList.remove("highlight");
+});
+
+const input = document.querySelector('input');
+
+input.addEventListener('change', function () {
+  console.log(input.value);
+});*/
